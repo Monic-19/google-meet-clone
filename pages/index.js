6e9c6ce49f0266039c7aba22df10a9dbe0b7a970
monic vyas <118365854+Monic-19@users.noreply.github.com>
2023-12-29 18:56:49 +0530

    Lobby Created

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,55 @@
-import { useSocket } from "@/context/socket"
-import { useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/navigation";
+import { SiGooglemeet } from "react-icons/si";
+import { MdKeyboardHide } from "react-icons/md";
+import { useState } from "react";
 
 export default function Home() {
 
-  const socket = useSocket();
-  
-  useEffect( () => {
-    socket?.on("connect", ()=>{
-      console.log(socket.id);
-    });
-  }, [socket])
+  const router = useRouter();
+
+  const [roomId, setRoomId] = useState("");
+
+  const createAndJoin = () => {
+    const roomId = uuidv4();
+    router.push(`/${roomId}`)
+  }
+
+  const joinRoom = () => {
+    if(roomId) router.push(`/${roomId}`)
+    else
+      alert("Please provide a valid room Id")
+  }
 
   return (
-    <>
-      <h1>Welcome</h1>
-    </>
+    <div className="hero">
+      <h1>Google Meet Clone</h1>
+
+      <div className="room">
+        <div className="left">
+
+          <div className="joinRoom">
+            <input placeholder="Enter a code" value={roomId} onChange={ (e) => setRoomId(e.target.value)}></input>
+             
+              <button className="btn btn1" onClick={joinRoom}> <SiGooglemeet></SiGooglemeet>
+              Join Room</button>
+
+          </div>
+
+          <h2>---------------------- OR ---------------------</h2>
+
+          <button className="btn btn2" onClick={createAndJoin}>
+            <MdKeyboardHide size={17}></MdKeyboardHide>
+            Create a new Room
+          </button>
+
+        </div>
+
+        <div className="right">
+        </div>
+      </div>
+
+
+    </div>
   )
 }
